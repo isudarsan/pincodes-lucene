2f1a3a3a4650a1ed418a2009da8543951ae5e2c5
Sudarshan <sudarsan.ambatipudi@capgemini.com>
2018-06-18 21:00:07 +0530

    Added NoSuchVillageException

---
+++ src/main/java/org/asnworks/apis/lucene/pincodes/exceptions/NoSuchVillageException.java
@@ -0,0 +1,14 @@
+
+package org.asnworks.apis.lucene.pincodes.exceptions;
+
+
+
+public class NoSuchVillageException extends Exception {
+
+    private static final long serialVersionUID = 3816644029300394590L;
+
+    public NoSuchVillageException(String message) {
+        super(message);
+    }
+
+}
