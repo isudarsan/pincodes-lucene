3d092056403a52e5a9726770f537fd1fe8b96a9d
Sudarsan <sudarsan.a@live.com>
2018-07-28 20:13:53 +0530

    Changed Solr Core Name
    Changed Solr Core Name

--- src/main/java/org/asnworks/apis/lucene/pincodes/constants/SolrConstants.java
+++ src/main/java/org/asnworks/apis/lucene/pincodes/constants/SolrConstants.java
@@ -4,7 +4,7 @@ package org.asnworks.apis.lucene.pincodes.constants;
 
 public class SolrConstants {
 
-    public static final String SOLR_CORE_NAME = "pincodes_core";
+    public static final String SOLR_CORE_NAME = "pincodes_db";
 
     public static final String VILLAGE_NAME = "village_name";
 
